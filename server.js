d8fda3fdb2e92c3fcf2e56329c22b78bf8e9d16d
Jacob Barnett <mrjbarnett@gmail.com>
2015-03-01 23:33:39 -0800

    turning in assignment

---
+++ server.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var express = require('express');
+var unicornRoutes = require('./routes/unicorn_routes');
+
+mongoose.connect(process.env.MONG_URI || 'mongodb://localhost/unicorn_corral_no_auth');
+
+var app = express();
+
+var unicornRouter = express.Router();
+
+unicornRoutes(unicornRouter, app.get('appSecret'));
+
+app.use('/api', unicornRouter);
+
+app.listen(process.env.PORT || 3000, function(){
+	console.log("server listening");
+});
